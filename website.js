f4240fb150f71cfbc37b1cef146ab94a246150c8
ZI0110 <79896438+ZI0110@users.noreply.github.com>
2021-05-13 00:06:18 +0800

    Add files via upload

---
+++ website.js
@@ -0,0 +1,153 @@
+
+
+//function myFunction1() {
+//  alert("A Girl Scout from Baguio City National High School (BCNHS) took over the mayoral seat at City Hall on Oct. 14 for the annual Scouts Official for a Day (SOFAD). \n\nScout Aevrylle Malihod, a Grade 10 student in the Science Technology and Engineering program, topped the examination of candidates held last Sept. 6 and the one-on-one interview on Sept. 10.\n\n“I would include strict implementation of proper waste management, incentive for honor students,and strict implementation of the anti-distraction law to the things I would like to work on in the future.”\n\nby: Enid Hope Maxigne D. Pinay-an");
+//}
+
+//function myFunction2() {
+//  var but2 = document.getElementById("myPopup");
+//  but2.classList.toggle("show");
+//}
+
+var cc2 = document.getElementById("c2");
+var btn = document.getElementById("bot2");
+var span = document.getElementsByClassName("close2")[0];
+btn.onclick = function() {
+  cc2.style.display = "block";
+}
+
+span.onclick = function() {
+  cc2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc2) {
+    cc2.style.display = "none";
+  }
+}
+
+var cc1 = document.getElementById("c1");
+var btn = document.getElementById("bot1");
+var span = document.getElementsByClassName("close1")[0];
+btn.onclick = function() {
+  cc1.style.display = "block";
+}
+
+span.onclick = function() {
+  cc1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc1) {
+    cc1.style.display = "none";
+  }
+}
+
+var cc3 = document.getElementById("c3");
+var btn = document.getElementById("bot3");
+var span = document.getElementsByClassName("close3")[0];
+btn.onclick = function() {
+  cc3.style.display = "block";
+}
+
+span.onclick = function() {
+  cc3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc3) {
+    cc3.style.display = "none";
+  }
+}
+
+var cc4 = document.getElementById("c4");
+var btn = document.getElementById("bot4");
+var span = document.getElementsByClassName("close4")[0];
+btn.onclick = function() {
+  cc4.style.display = "block";
+}
+
+span.onclick = function() {
+  cc4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc4) {
+    cc4.style.display = "none";
+  }
+}
+
+var cc5 = document.getElementById("c5");
+var btn = document.getElementById("bot5");
+var span = document.getElementsByClassName("close5")[0];
+btn.onclick = function() {
+  cc5.style.display = "block";
+}
+
+span.onclick = function() {
+  cc5.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc5) {
+    cc5.style.display = "none";
+  }
+}
+
+var cc6 = document.getElementById("c6");
+var btn = document.getElementById("bot6");
+var span = document.getElementsByClassName("close6")[0];
+btn.onclick = function() {
+  cc6.style.display = "block";
+}
+
+span.onclick = function() {
+  cc6.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc6) {
+    cc6.style.display = "none";
+  }
+}
+
+var cc7 = document.getElementById("c7");
+var btn = document.getElementById("bot7");
+var span = document.getElementsByClassName("close7")[0];
+btn.onclick = function() {
+  cc7.style.display = "block";
+}
+
+span.onclick = function() {
+  cc7.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc7) {
+    cc7.style.display = "none";
+  }
+}
+
+var cc8 = document.getElementById("c8");
+var btn = document.getElementById("bot8");
+var span = document.getElementsByClassName("close8")[0];
+btn.onclick = function() {
+  cc8.style.display = "block";
+}
+
+span.onclick = function() {
+  cc8.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cc8) {
+    cc8.style.display = "none";
+  }
+}
+
+
+
+
+function contactform() {
+    alert("Thank you for sharing your concern/ question. We will send our response in your email. Have a nice day. ");
+  }
